3be1ac15f2e28e8056280e0473efa877f03e9523
Senthil Kumar <324483552+senthilvsh@users.noreply.github.com>
2026-09-05 18:52:22 +0530

    Basic instructions

---
+++ src/Senthil.Comp.VirtualMachine/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Senthil.Comp.VirtualMachine;
+
+public static class VirtualMachine
+{
+    static byte[] mem =
+    {
+        0x01, // Load
+        0x00, // R0
+        0x02, // 2
+        0x01, // Load
+        0x01, // R1
+        0x03, // 3
+        0x03, // Add
+        0x00, // R0
+        0x01, // R1
+        0x00, // Halt
+    };
+
+    public static void Main()
+    {
+        byte[] R = { 0, 0 };
+        byte IP = 0;
+        bool running = true;
+        while (running)
+        {
+            byte instr = mem[IP];
+            switch (instr)
+            {
+                case 0x00:
+                    IP++;
+                    running = false;
+                    break;
+                case 0x01:
+                    byte regId = mem[IP + 1];
+                    byte val = mem[IP + 2];
+                    R[regId] = val;
+                    IP += 3;
+                    break;
+                case 0x03:
+                    byte regId1 = mem[IP + 1];
+                    byte regId2 = mem[IP + 2];
+                    R[regId1] += R[regId2];
+                    IP += 3;
+                    break;
+                default: throw new Exception("Unknown Instruction");
+            }
+        }
+
+        Console.WriteLine(R[0]);
+        Console.WriteLine(R[1]);
+    }
+}
